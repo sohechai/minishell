31ec57fcc632e73f35c6c66234b2a36b4bf70b31
Sofia Hechaïchi <sofiahechaichi@MacBook-Pro.local>
2021-02-19 22:39:18 +0100

    norm ft_exit

---
+++ srcs/cmd_func/ft_exit.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_exit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sofiahechaichi <sofiahechaichi@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/19 20:03:42 by sofiahechai       #+#    #+#             */
+/*   Updated: 2021/02/19 20:03:52 by sofiahechai      ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+int				ft_lenuntilspace(int i, char *str)
+{
+	int count;
+
+	count = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+		count++;
+		if (str[i] == ' ')
+			return (count);
+	}
+	return (count);
+}
+
+int				ft_exit(char *cmd, t_struct *st)
+{
+	int		i;
+	int		fd;
+
+	i = 0;
+	ft_printf("exit\n");
+	if (st->redirection == DOUBLERED || st->redirection == SIMPLERED)
+		fd = open(st->newfd, O_CREAT | O_RDWR | O_APPEND, 0640);
+	while (cmd[i])
+	{
+		if (cmd[i] == ' ')
+		{
+			while (cmd[i] == ' ')
+				i++;
+			if (ft_isalpha(cmd[i]) != 0)
+			{
+				ft_printf("exit: %s : argument numérique nécessaire\n",
+						ft_substr(cmd, i, ft_lenuntilspace(i, cmd)));
+				break ;
+			}
+		}
+		i++;
+	}
+	//ft_freetab(st->copyenvp);
+	ft_freestruct(st);
+	// TODO faire fonction ft_exit qui free proprement tout ce qui a ete alloué
+	exit(st->exitstatus);
+	return (EXIT_SUCCESS);
+}
--- srcs/cmd_func/ft_simplecmd.c
+++ srcs/cmd_func/ft_simplecmd.c
@@ -6,57 +6,11 @@
 /*   By: sofiahechaichi <sofiahechaichi@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 15:03:46 by sofiahechai       #+#    #+#             */
-/*   Updated: 2021/02/17 23:19:41 by sofiahechai      ###   ########lyon.fr   */
+/*   Updated: 2021/02/19 20:03:53 by sofiahechai      ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-int				ft_lenuntilspace(int i, char *str)
-{
-	int count;
-
-	count = 0;
-	while(str[i] != '\0')
-	{
-		i++;
-		count++;
-		if (str[i] == ' ')
-			return (count);
-	}
-	return (count);
-}
-
-int				ft_exit(char *cmd, t_struct *st)
-{
-	int		i;
-	int		fd;
-
-	i = 0;
-	ft_printf("exit\n");
-	if (st->redirection == DOUBLERED || st->redirection == SIMPLERED)
-		fd = open(st->newfd, O_CREAT | O_RDWR | O_APPEND, 0640);
-	while (cmd[i])
-	{
-		if (cmd[i] == ' ')
-		{
-			while (cmd[i] == ' ')
-				i++;
-			if (ft_isalpha(cmd[i]) != 0)
-			{
-				ft_printf("exit: %s : argument numérique nécessaire\n",
-						ft_substr(cmd, i, ft_lenuntilspace(i, cmd)));
-				break ;
-			}
-		}
-		i++;
-	}
-	//ft_freetab(st->copyenvp);
-	ft_freestruct(st);
-	// TODO faire fonction ft_exit qui free proprement tout ce qui a ete alloué
-	exit(st->exitstatus);
-	return (EXIT_SUCCESS);
-}
 
 int				ft_checkquote(char *str)
 {
